d13d15707c85e6e3545afd4f094a2c5384c77568
jbmattos03 <jbmattos2003@gmail.com>
2025-12-31 02:00:22 -0300

    feat(backend): Add login function to UserService
    Also improved error handling and removed .js extension from imports

--- recarrega_backend/src/Services/userService.ts
+++ recarrega_backend/src/Services/userService.ts
@@ -1,5 +1,8 @@
-import User from "../Models/userModel.js"
+import User from "../Models/userModel"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config()
 
 class UserService {
     // CRUD
@@ -8,7 +11,10 @@ class UserService {
             // Check if email is already being used
             const existingUser = await User.findOne({ where: { email: email, isDeleted: false } });
             if (existingUser) {
-                throw new Error("E-mail is already in use")
+                const err =  new Error("E-mail is already in use");
+                err.name = "UserExistsError";
+                throw err;
+
             }
 
             // Hashing the password
@@ -18,7 +24,7 @@ class UserService {
             const user = await User.create({
                 name: name,
                 email: email,
-                password: hashedPassword
+                password: hashedPassword,
             });
             return user;
         } catch (error) {
@@ -32,12 +38,16 @@ class UserService {
             // Check if user exists
             const user = await User.findOne({ where: { id: userId, isDeleted: false } });
             if (!user) {
-                throw new Error("User not found");
+                const err =  new Error("User not found");
+                err.name = "UserNotFoundError";
+                throw err;
             }
 
             // Check if any attributes were provided
             if (!(name || email)) {
-                throw new Error("At least one attribute must be provided")
+                const err = new Error("At least one attribute must be provided");
+                err.name = "MissingAttributesError";
+                throw err;
             }
 
             let updateData: Record<string, any> = {};
@@ -46,6 +56,14 @@ class UserService {
                 updateData.name = name;
             }
             if (email) {
+                // Check if a user with this email already exists
+                const existinguser = await User.findOne({ where: { email: email } });
+                if (existinguser) {
+                    const err =  new Error("E-mail already in use");
+                    err.name = "UserExistsError";
+                    throw err;
+                }
+                
                 updateData.email = email;
             }
 
@@ -65,7 +83,9 @@ class UserService {
         try {
             const user = await User.findOne({ where: { id: userId, isDeleted: false } });
             if (!user) {
-                throw new Error("User not found");
+                const err =  new Error("User not found");
+                err.name = "UserNotFoundError";
+                throw err;
             }
             
             // 'Delete' user: soft delete
@@ -76,18 +96,55 @@ class UserService {
         }
     }
 
+    // Login
+    static async loginUser(email: string, password: string): Promise<string> {
+        try {
+            // Checking if user exists
+            const user = await User.findOne({ where: { email: email } });
+            if (!user) {
+                const err = new Error("User not found");
+                err.name = "UserNotFoundError";
+                throw err;
+            }
+
+            // Comparing stored password with received password
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                // Creating jwt token
+                const token = jwt.sign({
+                    id: user.id,
+                    email: user.email,
+                },
+                process.env.JWT_SECRET!, // Trust me bro; if JWT_SECRET hasn't been set, it will get caught before runtime gets here
+                { expiresIn: "1h" }
+                );
+
+                return token;
+            } else {
+                const err = new Error("Invalid email or password");
+                err.name = "InvalidCredentialsError";
+                throw err;
+            }
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : "An unknown error has occurred";
+            throw new Error(`Error logging user in: ${errorMessage}`);
+        }
+    }
+
     // 'Find by' functions
     static async findUserByPk(userId: number): Promise<User> {
         try {
             const user = await User.findOne({ where: { id: userId, isDeleted: false } });
             if (!user) {
-                throw new Error("User not found");
+                const err = new Error("User not found");
+                err.name = "UserNotFoundError";
+                throw err;
             }
 
             return user;
         } catch (error) {
             const errorMessage = error instanceof Error ? error.message : "An unknown error has occurred";
-            throw new Error(`Error fetching user by pk: ${errorMessage}`);
+            throw new Error(`Error fetching user: ${errorMessage}`);
         }
     }
 
@@ -95,13 +152,15 @@ class UserService {
         try {
             const user = await User.findOne({ where: { email: email, isDeleted: false } });
             if (!user) {
-                throw new Error("User not found");
+                const err = new Error("User not found");
+                err.name = "UserNotFoundError";
+                throw err;
             }
 
             return user;
         } catch (error) {
             const errorMessage = error instanceof Error ? error.message : "An unknown error has occurred";
-            throw new Error(`Error fetching user by email: ${errorMessage}`);
+            throw new Error(`Error fetching user: ${errorMessage}`);
         }
     }
 }
